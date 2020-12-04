5da212f12070461577e4d1d980429ee1c8b0a9dd
RitikaRoy2020 <roy.ritika2016@gmail.com>
2020-12-04 10:27:03 +0530

    commit twice

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ export default class Form1 extends React.Component {
     this.setState({name: event.target.value});
   }
 
+  ageChangeHandler = (event) => {
+    this.setState({age: event.target.value});
+  }
+
+  addressChangeHandler = (event) => {
+    this.setState({address: event.target.value});
+  }
+
+  phnumChangeHandler = (event) => {
+    this.setState({num: event.target.value});
+  }
+
   submitHandler = (event) => {
     event.preventDefault();
     axios.post('http://localhost:8080/test/customer', {
@@ -32,14 +44,33 @@ export default class Form1 extends React.Component {
   render() {
     return(
       <form onSubmit = {this.submitHandler}>
-        <h1>Hello</h1>
+        <select value = {this.state.cusid}>
+          <option value = "01">Customer Id: 01</option>
+          <option value = "02">Customer Id: 02</option>
+          <option value = "03">Customer Id: 03</option>
+        </select>
+        <h1>Enter Your Details</h1>
         <p>Enter your name: </p>
         <input
           type = "text"
           onChange = {this.nameChangeHandler}
-          />
+        />
+        <p>Enter your age: </p>
+        <input
+          type = "text"
+          onChange = {this.ageChangeHandler}
+        />
+        <p>Enter your address: </p>
+        <input
+          type = "text"
+          onChange = {this.addressChangeHandler}/>
+        <p>Enter your Phone Number</p>
+        <input
+          type = "text"
+          onChange = {this.phnumChangeHandler}/>
         <p>
-          <input type='submit' />
+          <link rel= "stylesheet" type="text/css" href= "index.css"></link>
+          <button type= "button">submit</button>
         </p>
       </form>
     );
@@ -50,3 +81,5 @@ ReactDOM.render(
   < Form1/>,
   document.getElementById('root')
 );
+
+//ReactDOM.render(document.getElementById('source'));
