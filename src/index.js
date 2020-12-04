d7ea912b3255745379d99b36bb1a92d854757274
RitikaRoy2020 <roy.ritika2016@gmail.com>
2020-12-04 19:59:00 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -49,6 +49,11 @@ export default class Form1 extends React.Component {
           <option value = "02">Customer Id: 02</option>
           <option value = "03">Customer Id: 03</option>
         </select>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
         <h1>Enter Your Details</h1>
         <p>Enter your name: </p>
         <input
